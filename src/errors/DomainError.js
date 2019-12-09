f70b4939eb96e48e9de9b1d2c099fa0a8f385f89
Teddy Andersson <teddyandersson@Teddys-MacBook-Pro.local>
2019-12-09 16:20:47 +0100

    Fixed missing import

--- src/errors/DomainError.js
+++ src/errors/DomainError.js
@@ -1,3 +1,4 @@
+const errorMessages = require('../../../assets/errorMessages')
 
 /**
  * DomainError
@@ -14,7 +15,7 @@ class DomainError extends Error {
       // Ensure the name of this error is the same as the class name
       this.name = this.constructor.name;
       this.status = status;
-      this.detail = errorMessage[status];
+      this.detail = errorMessages[status];
       this.message = msg
       // This clips the constructor invocation from the stack trace.
       // It's not absolutely essential, but it does make the stack trace a little nicer.
