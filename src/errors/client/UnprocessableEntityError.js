d15964f2c13977843899e81a62f863650dbbad3c
Teddy Andersson <teddyandersson95@gmail.com>
2019-12-10 14:15:42 +0100

    Added support for eslint (#2)
    * Added support for eslint
    
    * Resolved Conflicts
    
    * Fixed linting errors and elsintrc setup

--- src/errors/client/UnprocessableEntityError.js
+++ src/errors/client/UnprocessableEntityError.js
@@ -1,22 +1,22 @@
-const DomainError = require('../DomainError')
-const errorMessages = require('../../../assets/errorMessages')
+const DomainError = require('../DomainError');
+const errorMessages = require('../../../assets/errorMessages');
 
 /**
  * 422: UNPROCESSABLE ENTITY ERROR
- * 
+ *
  * The server understands the content type of the request entity
- * (hence a 415 Unsupported Media Type status code is inappropriate), 
- * and the syntax of the request entity is correct (thus a 400 Bad 
- * Request status code is inappropriate) but was unable to process 
+ * (hence a 415 Unsupported Media Type status code is inappropriate),
+ * and the syntax of the request entity is correct (thus a 400 Bad
+ * Request status code is inappropriate) but was unable to process
  * the contained instructions.
- * 
+ *
  * Source: RFC4918 Section 11.2
  */
 
 class UnprocessableEntityError extends DomainError {
-    constructor(msg = errorMessages[422]) {
-      super(msg, 422);
-    }
+  constructor(msg = errorMessages[422]) {
+    super(msg, 422);
+  }
 }
 
 module.exports = UnprocessableEntityError;
--- src/errors/index.js
+++ src/errors/index.js
@@ -2,6 +2,6 @@ const clientErrors = require('./client');
 const serverErrors = require('./server');
 
 module.exports = {
-    ...clientErrors,
-    ...serverErrors
-}
+  ...clientErrors,
+  ...serverErrors,
+};
